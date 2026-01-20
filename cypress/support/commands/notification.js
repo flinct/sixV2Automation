941daba8250e6ec727a06c8ec7d823386f469336
syncm09 <81354940+flinct@users.noreply.github.com>
2026-01-20 09:50:11 +0700

    update yml

--- cypress/support/commands/notification.js
+++ cypress/support/commands/notification.js
@@ -1,9 +1,12 @@
 class elementToast {
   toastError() {
-    return cy.get(".Toastify__toast--error");
+    // return cy.get(".Toastify__toast--error");
+    // return cy.get(".Toastify__toast-container");
+    return cy.get(".Toastify__toast");
+    //Toastify__toast-container Toastify__toast-container--bottom-right
   }
   toastErrorMsg() {
-    return cy.get(".Toastify__toast--error");
+    return cy.get(".Toastify__toast").find("div");
     // return cy.get(".Toastify__toast--error").find("h1");
   }
   toastErrorTitle() {
